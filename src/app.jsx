7366ef58a0f94d401580bb36155f04b6d433fabb
MarcioBADias <marcio.b.a.dias@gmail.com>
2024-01-16 00:02:24 -0300

    Iniciando Projeto

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,26 @@
 const App = () => {
   return (
     <>
-      <h1>Boilerplace + React</h1>
+      <nav className="navbar">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Especialidades</a>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Unidades
+            </a>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href="#">Cabo Fio - São Cristovão</a></li>
+              <li><a className="dropdown-item" href="#">Cabo Frio - Passagem</a></li>
+              <li><a className="dropdown-item" href="#">São Pedro da Aldeia</a></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
     </>
   )
 }
