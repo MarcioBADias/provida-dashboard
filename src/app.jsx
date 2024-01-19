a9b3037a4e3b06ec8cd41fc0c04f420521847c3b
MarcioBADias <marcio.b.a.dias@gmail.com>
2024-01-19 00:00:00 -0300

    ⚠ Preparando os espacos para criar os cards e funcoes

--- src/app.jsx
+++ src/app.jsx
@@ -1,37 +1,19 @@
-import { useState } from "react"
+import { Menu } from "./components/menu"
+
 
 const App = () => {
-  const [openDropdown, setOpenDropdow] = useState(false)
-
-  const handleClickDropdown = () => setOpenDropdow(prev => !prev)
   return (
     <>
-      <nav className="navbar">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Especialidades</a>
-          </li>
-          <li className="nav-item dropdown" onClick={handleClickDropdown}>
-            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Unidades
-            </a>
-            {
-              openDropdown &&
-              <ul className="dropdown-menu">
-                <li><a className="dropdown-item" href="#">Cabo Fio - São Cristovão</a></li>
-                <li><a className="dropdown-item" href="#">Cabo Frio - Passagem</a></li>
-                <li><a className="dropdown-item" href="#">São Pedro da Aldeia</a></li>
-              </ul>}
-          </li>
-        </ul>
-      </nav>
-
-      <main>
-        <h1>Testes</h1>
-      </main>
+      <Menu />
+      <div className="container">
+        <h1>Convenios</h1>
+      </div>
+      <div className="container">
+        <h1>Medicos</h1>
+      </div>
+      <div className="container">
+        <h1>Exames</h1>
+      </div>
     </>
   )
 }
