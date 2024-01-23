3d3e23a40ad0f27842420cfa60cf7ffb8ba1d491
MarcioBADias <marcio.b.a.dias@gmail.com>
2024-01-23 07:30:10 -0300

    Quebrando componentes no click

--- src/app.jsx
+++ src/app.jsx
@@ -1,23 +1,41 @@
+import { useState } from "react"
 import { Menu } from "./components/menu"
 
 const App = () => {
+  const [selectBox, setSelectBox] = useState(true)
+  const [newsLetter, setNewLetter] = useState(false)
+  //const [convenios, setConvenios] = useState(false)
+  //const [medicos, setMedicos] = useState(false)
+  //const [exames,setExames] = useState(false)
+
+  const handleClickInBox = (e) => {
+    if(e.target.className.includes('newsLetter')) {
+      setSelectBox(prev => !prev)
+      setNewLetter(prev => !prev)
+    }
+  }
   return (
     <>
       <Menu />
-      <div className="container">
-        <div className="newsLetter">
-          
-        </div>
+
+      <div className="container" onClick={handleClickInBox}>
+      {selectBox && <div className="newsLetter">
+          {
+            newsLetter && <div className="container newsLetter">
+              <h1>Pagina de noticias</h1>
+            </div>
+          }
+        </div>}
         <div className="optionBox">
-          <div className="convenios box">
+        {selectBox && <div className="convenios box">
             
-          </div>
-          <div className="medicos box">
+          </div>}
+          {selectBox && <div className="medicos box">
             
-          </div>
-          <div className="exames box">
+          </div>}
+          {selectBox && <div className="exames box">
             
-          </div>
+          </div>}
         </div>
       </div>
     </>
