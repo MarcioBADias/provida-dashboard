eca183dee09cc84809746f402d7282686841f64c
MarcioBADias <marcio.b.a.dias@gmail.com>
2024-02-11 02:36:10 -0300

    ✅ Marcação inicial

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,26 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app.jsx'
 import './index.css'
-import { ConveniosPage } from "./pages/conveniosPage.jsx"
-import { MedicosPage } from "./pages/medicos-page/medicosPage.jsx"
-import { ExPage } from "./pages/exames-page/examesPage.jsx"
-import { NewsLetterPage } from "./pages/newsLetterPage.jsx"
+import { ConveniosPage } from './pages/montagem-de-salas/conveniosPage.jsx'
+import { MedicosPage } from './pages/medicos-page/medicosPage.jsx'
+import { ExPage } from './pages/exames-page/examesPage.jsx'
+import { NewsLetterPage } from './pages/newsLetterPage.jsx'
 
 const rootElement = document.querySelector('[data-js="root"]')
-const root = createRoot(rootElement)  
+const root = createRoot(rootElement)
 
 root.render(
-    <StrictMode>
-        
+  <StrictMode>
     <Router>
-        <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/convenios" element={<ConveniosPage />} />
-            <Route path="/medicos" element={<MedicosPage />} />
-            <Route path="/exames" element={<ExPage />} />
-            <Route path="/newsLetter" element={<NewsLetterPage />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/convenios" element={<ConveniosPage />} />
+        <Route path="/medicos" element={<MedicosPage />} />
+        <Route path="/exames" element={<ExPage />} />
+        <Route path="/newsLetter" element={<NewsLetterPage />} />
+      </Routes>
     </Router>
-    </StrictMode>
-    ,)
+  </StrictMode>,
+)
---
+++ src/pages/montagem-de-salas/style.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const FormSalas = styled.form`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`
+
+export const InputSalas = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`
+export const TableArea = styled.div`
+  display: flex;
+  gap: 1rem;
+  justify-content: space-around;
+  margin-top: 2rem;
+`
